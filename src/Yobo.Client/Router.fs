e9e95ea45508816bdf732a4090c9a5b2e5d72cc9
Roman Provazník <dzoukr@dzoukr.cz>
2019-05-13 09:52:59 +0200

    Cleanup

--- src/Yobo.Client/Router.fs
+++ src/Yobo.Client/Router.fs
@@ -1,23 +1,10 @@
 module Yobo.Client.Router
 
 open System
-open Elmish.Browser.UrlParser
 open Elmish.Browser.Navigation
 open Fable.Import
 open Fable.Helpers
 open Fable.Core.JsInterop
-
-//module Routes =
-    //let defaultPage = "/calendar"
-    //let login = "/login"
-    //let registration = "/registration"
-    //let accountActivation = "/accountActivation"
-    //let users = "/users"
-    //let lessons = "/lessons"
-    //let calendar = "/calendar"
-    //let forgottenPassword = "/forgottenPassword"
-    //let resetPassword = "/resetPassword"
-    //let mylessons = "/mylessons"
 
 type AuthPage =
     | Login
